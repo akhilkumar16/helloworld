11d1361ed53dbf483e35007ffcfac8c9c5dc6b68
unknown <akhilkumarchandragiri@gmail.com>
2021-12-15 09:45:24 +0530

    first program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+   public class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HelloWorld/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class student
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("student name ");
+        }
+        
+    }
+}
